c4caea585f372d8ee1076ebe862de18a6cac9e8b
Cássio Leite <cassiocarmo77@gmail.com>
2026-07-14 01:33:44 -0300

    feat: integrate Interests section into home route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Header } from '#/components/layout/Header'
 import { Home as HomeSection } from '#/components/sections/Home'
 import { About as AboutSection } from '#/components/sections/About'
+import { Interests as InterestsSection } from '#/components/sections/Interests'
 
 export const Route = createFileRoute('/')({ component: RouteComponent })
 
@@ -12,6 +13,7 @@ function RouteComponent() {
       <main>
         <HomeSection />
         <AboutSection />
+        <InterestsSection />
       </main>
     </>
   )
